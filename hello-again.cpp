9f4bb4f7f58b98c0a08bbf7d0e1285d84d1ac2d2
Sat Garcia <sat@sandiego.edu>
2019-02-10 21:03:06 -0800

    more comments and simplified first usage of cout

--- hello-again.cpp
+++ hello-again.cpp
@@ -6,5 +6,13 @@
 
 int main() {
 	int version = 2;
+
+	std::cout << "Hello C++, take "; // << is the "insertion" operator
+	std::cout << version;
+	std::cout << "\n";
+
+	version++;
+
+	// you can combine multiple insertions into a single line
 	std::cout << "Hello C++, take " << version << "\n";
 }
